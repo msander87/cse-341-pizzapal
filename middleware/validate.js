8d660a2104503d9eee58f7441cca1e82e7aeb77f
natelandrum <nate178dawg@gmail.com>
2024-05-30 14:47:03 -0600

    Add menu item validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -48,10 +48,27 @@ const saveOrder = (req, res, next) => {
             next();
         }
     });
-   
+};
+
+const validateMenuItem = (req, res, next) => {
+    const validationRule = {
+        name: 'required|string',
+    };
+    validator(req.body, validationRule, {}, (err, status) =>{
+        if(!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
 };
 
 module.exports = {
     saveOrder,
-    saveUser
+    saveUser,
+    validateMenuItem
 }
