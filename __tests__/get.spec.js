f4256468691db6de39598e378093cf9d16baf022
natelandrum <nate178dawg@gmail.com>
2024-06-01 19:18:44 -0600

    Add failing route tests

--- __tests__/get.spec.js
+++ __tests__/get.spec.js
@@ -55,6 +55,20 @@ describe('Test Handlers', () => {
         expect(res.text).toBe('[{"_id":"66593f292b3daf9995c1cbb9","product":"Hawaiian"}]');
     }),
 
+    test('responds to /menu/product/:id', async () => {
+        const res = await request.get('/menu/product/1');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(400);
+        expect(res.text).toBe('"Must use a valid product id"');
+    }),
+
+    test('responds to /menu/product/:id', async () => {
+        const res = await request.get('/menu/product/66593f292b3daf9995c1cbb8');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(404);
+        expect(res.text).toBe('"Product not found"');
+    }),
+
     test('responds to /menu/topping/', async () => {
         const res = await request.get('/menu/topping/');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
@@ -66,6 +80,20 @@ describe('Test Handlers', () => {
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe('[{"_id":"665944e5d7c48bdf4e6ae9b6","topping":"Pepperoni"}]');
+    }),
+
+    test('responds to /menu/topping/:id', async () => {
+        const res = await request.get('/menu/topping/665944e5d7c48bdf4e6ae9b7');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(404);
+        expect(res.text).toBe('"Topping not found"');
+    }),
+
+    test('responds to /menu/topping/:id', async () => {
+        const res = await request.get('/menu/topping/1');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(400);
+        expect(res.text).toBe('"Must use a valid topping id"');
     }),
 
     test('responds to /menu/size/', async () => {
@@ -81,6 +109,20 @@ describe('Test Handlers', () => {
         expect(res.text).toBe('[{"_id":"66594599d7c48bdf4e6ae9c1","size":"Small"}]');
     }),
 
+    test('responds to /menu/size/:id', async () => {
+        const res = await request.get('/menu/size/66594599d7c48bdf4e6ae9c2');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(404);
+        expect(res.text).toBe('"Size not found"');
+    }),
+
+    test('responds to /menu/size/:id', async () => {
+        const res = await request.get('/menu/size/1');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(400);
+        expect(res.text).toBe('"Must use a valid size id"');
+    }),
+
     test('responds to /menu/crust/', async () => {
         const res = await request.get('/menu/crust/');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
@@ -92,5 +134,19 @@ describe('Test Handlers', () => {
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe('[{"_id":"6659488fd7c48bdf4e6ae9c8","crust":"Flatbread"}]');
+    }),
+
+    test('responds to /menu/crust/:id', async () => {
+        const res = await request.get('/menu/crust/6659488fd7c48bdf4e6ae9c9');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(404);
+        expect(res.text).toBe('"Crust not found"');
+    }),
+
+    test('responds to /menu/crust/:id', async () => {
+        const res = await request.get('/menu/crust/1');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(400);
+        expect(res.text).toBe('"Must use a valid crust id"');
     })
 })
